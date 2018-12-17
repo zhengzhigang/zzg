40f6e26f352937abe09caaee80438d32e2d6e2af
zhengzhigang <zhengzhigang@igengmei.com>
2018-12-17 16:38:12 +0800

    base64 修改

--- node/utils/base64.js
+++ node/utils/base64.js
@@ -2,24 +2,26 @@
  * 定义BASE64加解密的类
  * @constructor
  */
-function Base64() {
-    // 安全工程方法
-    if (!(this instanceof Base64)) {
-        return new Base64();
-    }
-    // private property
-    let _keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-    /**
+ class Base64 {
+     constructor() {
+        // 安全工程方法
+        if (!(this instanceof Base64)) {
+            return new Base64();
+        }
+        this._keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+     }
+
+     /**
      * 定义加密方法
      * @param {[type]} input [description]
      * @return {[type]}      [description]
      */
-    this.encode = function (input) {
+    encode(input) {
         let output = '';
         let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
         let i = 0;
-        input = _utf8_encode(input);
+        input = this._utf8_encode(input);
         while (i < input.length) {
             chr1 = input.charCodeAt(i++);
             chr2 = input.charCodeAt(i++);
@@ -34,28 +36,27 @@ function Base64() {
                 enc4 = 64;
             }
             output = output +
-                _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
-                _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+                this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+                this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
         }
         return output;
     }
-
     /**
      * 定义解密的方法
      * @param  {[type]} input [description]
      * @return {[type]}       [description]
      */
-    this.decode = function (input) {
+    decode(input) {
         var output = "";
         var chr1, chr2, chr3;
         var enc1, enc2, enc3, enc4;
         var i = 0;
         input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
         while (i < input.length) {
-            enc1 = _keyStr.indexOf(input.charAt(i++));
-            enc2 = _keyStr.indexOf(input.charAt(i++));
-            enc3 = _keyStr.indexOf(input.charAt(i++));
-            enc4 = _keyStr.indexOf(input.charAt(i++));
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
             chr1 = (enc1 << 2) | (enc2 >> 4);
             chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
             chr3 = ((enc3 & 3) << 6) | enc4;
@@ -67,12 +68,12 @@ function Base64() {
                 output = output + String.fromCharCode(chr3);
             }
         }
-        output = _utf8_decode(output);
+        output = this._utf8_decode(output);
         return output;
     }
 
     // private method for UTF-8 encoding
-    let _utf8_encode = function(str) {
+    _utf8_encode(str) {
         str = str.replace(/\r\n/g, '\n');
         let utftext = '';
         for (let n = 0; n < str.length; n++) {
@@ -92,7 +93,7 @@ function Base64() {
     }
 
     // private method for UTF-8 decoding
-    let _utf8_decode = function (utftext) {
+    _utf8_decode(utftext) {
         var string = "";
         var i = 0,
             c = c2,
@@ -116,6 +117,122 @@ function Base64() {
         }
         return string;
     }
-}
+
+ }
+// function Base64() {
+//     // 安全工程方法
+//     if (!(this instanceof Base64)) {
+//         return new Base64();
+//     }
+//     // private property
+//     let _keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+//     /**
+//      * 定义加密方法
+//      * @param {[type]} input [description]
+//      * @return {[type]}      [description]
+//      */
+//     this.encode = function (input) {
+//         let output = '';
+//         let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+//         let i = 0;
+//         input = _utf8_encode(input);
+//         while (i < input.length) {
+//             chr1 = input.charCodeAt(i++);
+//             chr2 = input.charCodeAt(i++);
+//             chr3 = input.charCodeAt(i++);
+//             enc1 = chr1 >> 2;
+//             enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+//             enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+//             enc4 = chr3 & 63;
+//             if (isNaN(chr2)) {
+//                 enc3 = enc4 = 64;
+//             } else if (isNaN(chr3)) {
+//                 enc4 = 64;
+//             }
+//             output = output +
+//                 _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+//                 _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+//         }
+//         return output;
+//     }
+
+//     /**
+//      * 定义解密的方法
+//      * @param  {[type]} input [description]
+//      * @return {[type]}       [description]
+//      */
+//     this.decode = function (input) {
+//         var output = "";
+//         var chr1, chr2, chr3;
+//         var enc1, enc2, enc3, enc4;
+//         var i = 0;
+//         input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+//         while (i < input.length) {
+//             enc1 = _keyStr.indexOf(input.charAt(i++));
+//             enc2 = _keyStr.indexOf(input.charAt(i++));
+//             enc3 = _keyStr.indexOf(input.charAt(i++));
+//             enc4 = _keyStr.indexOf(input.charAt(i++));
+//             chr1 = (enc1 << 2) | (enc2 >> 4);
+//             chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+//             chr3 = ((enc3 & 3) << 6) | enc4;
+//             output = output + String.fromCharCode(chr1);
+//             if (enc3 != 64) {
+//                 output = output + String.fromCharCode(chr2);
+//             }
+//             if (enc4 != 64) {
+//                 output = output + String.fromCharCode(chr3);
+//             }
+//         }
+//         output = _utf8_decode(output);
+//         return output;
+//     }
+
+//     // private method for UTF-8 encoding
+//     let _utf8_encode = function(str) {
+//         str = str.replace(/\r\n/g, '\n');
+//         let utftext = '';
+//         for (let n = 0; n < str.length; n++) {
+//             let c = str.charCodeAt(n);
+//             if (c < 128) {
+//                 utftext += String.fromCharCode(c)
+//             } else if (c > 127 && c < 2048) {
+//                 utftext += String.fromCharCode((c >> 6) | 192);
+//                 utftext += String.fromCharCode((c & 63) | 128);
+//             } else {
+//                 utftext += String.fromCharCode((c >> 12) | 224);
+//                 utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+//                 utftext += String.fromCharCode((c & 63) | 128);
+//             }
+//         }
+//         return utftext;
+//     }
+
+//     // private method for UTF-8 decoding
+//     let _utf8_decode = function (utftext) {
+//         var string = "";
+//         var i = 0,
+//             c = c2,
+//             c2 = c3,
+//             c3 = 0;
+//         while ( i < utftext.length ) {
+//             c = utftext.charCodeAt(i);
+//             if (c < 128) {
+//                 string += String.fromCharCode(c);
+//                 i++;
+//             } else if((c > 191) && (c < 224)) {
+//                 c2 = utftext.charCodeAt(i+1);
+//                 string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+//                 i += 2;
+//             } else {
+//                 c2 = utftext.charCodeAt(i+1);
+//                 c3 = utftext.charCodeAt(i+2);
+//                 string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+//                 i += 3;
+//             }
+//         }
+//         return string;
+//     }
+// }
 
 export default Base64;
